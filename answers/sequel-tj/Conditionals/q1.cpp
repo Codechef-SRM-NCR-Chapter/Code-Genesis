b8a82283fc13d8731a67c4c8967aa882af0470fa
sequel-tj <74481733+sequel-tj@users.noreply.github.com>
2021-06-26 17:48:22 +0530

    create q1.cpp

--- answers/sequel-tj/Conditionals/q1.cpp
+++ answers/sequel-tj/Conditionals/q1.cpp
@@ -2,19 +2,17 @@
 using namespace std;
 
 int main() {
-    int basic,allow;
-    char alph;
-    cin >> basic >> alph;
+    double basic,allow;
+    char grade;
+    cin >> basic >> grade;
 
-    if (alph == 'A')
+    if (grade == 'A')
         allow = 1700;
-    else if (alph == 'B')
+    else if (grade == 'B')
         allow = 1500;
     else
         allow = 1300;
 
-    int total_salary = basic + (int)(0.20 * basic) + (int)(0.50 * basic) + allow - (int)(0.11 * basic);
-    cout << total_salary << endl;
-    
+    cout << basic + (int)(0.20 * basic) + (int)(0.50 * basic) + allow - (int)(0.11 * basic) << endl;
     return 0;
 }
