dee586ef88ec8493208998ea47b1c4d3afdfa429
Kshitijain123 <86479092+Kshitijain123@users.noreply.github.com>
2021-06-26 16:30:51 +0530

    add solutions for conditional

---
+++ kshiti jain/day1/question 5.c
@@ -0,0 +1,50 @@
+# include <stdio.h>
+
+int main ()
+
+{
+    int a,b,sum,sub,multi,div;
+ 
+   char operator;
+ 
+   printf("enter the numbers a and b");
+  
+  scanf("%d %d",&a,&b);
+ 
+   printf("enter the operator");
+ 
+   scanf("%c",& operator);
+ 
+   if(operator='+')
+
+{  sum=a+b;
+
+printf("the sum of two numbers is %d \n",sum);
+
+
+    }
+  
+  if (operator ='-')
+  
+  {sub=a-b;
+
+    printf("the difference of two numbers is %d \n",sub);}
+  
+  if(operator='*')
+ 
+   {
+        multi=a*b;
+  
+      printf("the product of two numbers is %d \n",multi);
+    }
+ 
+   if(operator='/')
+ 
+   {
+        div=a/b ;
+      
+  printf("the divide of two numbers is %d \n",div);
+        }
+    
+
+}
