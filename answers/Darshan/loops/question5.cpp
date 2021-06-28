4d676cf8d37693bda147eae825cc2701fea3d954
Darshan <dhulswar591@gmail.com>
2021-06-28 11:57:13 +0530

    updated solution for question 5

--- answers/Darshan/loops/question5.cpp
+++ answers/Darshan/loops/question5.cpp
@@ -14,9 +14,5 @@ int main() {
     sort(arr, arr + n);
     reverse(arr, arr + n);
 
-    for (int i = 0; i < n; i++) {
-        cout << arr[i];
-    }
-
     cout << "Second largest element: " << arr[1];
 }
