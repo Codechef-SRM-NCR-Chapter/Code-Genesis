ccab9d5fb5d792e9d1a68c290b2a2646ac924485
Darshan <dhulswar591@gmail.com>
2021-06-28 18:05:07 +0530

    question5 solved

--- answers/Darshan/loops/question5.cpp
+++ answers/Darshan/loops/question5.cpp
@@ -8,9 +8,8 @@ int main() {
     cin >> n;
     int arr[n];
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
         cin >> arr[i];
-    }
 
     if(n == 2 || n == 1) {
         cout << INT_MIN;
