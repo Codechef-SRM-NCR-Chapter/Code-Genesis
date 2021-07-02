8c6e8da0a465f7edbb8dfdd5a32aeefbd5b8c967
prajavisharma <81108503+prajavisharma@users.noreply.github.com>
2021-07-02 17:23:38 +0530

    Add files via upload

--- answers/prajavisharma/Q2.c
+++ answers/prajavisharma/Q2.c
@@ -1,85 +1,20 @@
 
-
+  	#include <math.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
 
-
-
-struct pairSum {
-
-	
-	int first;
-
-	
-	int sec;
-
-	int sum;
-};
-
-
-int compare(const void* a, const void* b)
-{
-	return ((*(pairSum*)a).sum - (*(pairSum*)b).sum);
-}
-
-
-bool noCommon(struct pairSum a, struct pairSum b)
-{
-	if (a.first == b.first || a.first == b.sec
-		|| a.sec == b.first || a.sec == b.sec)
-		return false;
-	return true;
-}
-
-
-void findFourElements(int arr[], int n, int X)
-{
-	int i, j;
-
-
-	int size = (n * (n - 1)) / 2;
-	struct pairSum aux[size];
-
-
-	int k = 0;
-	for (i = 0; i < n - 1; i++) {
-		for (j = i + 1; j < n; j++) {
-			aux[k].sum = arr[i] + arr[j];
-			aux[k].first = i;
-			aux[k].sec = j;
-			k++;
-		}
-	}
-
-	
-	qsort(aux, size, sizeof(aux[0]), compare);
-
-	
-	i = 0;
-	j = size - 1;
-	while (i < size && j >= 0) {
-		if ((aux[i].sum + aux[j].sum == X)
-			&& noCommon(aux[i], aux[j])) {
-			printf("%d, %d, %d, %d\n", arr[aux[i].first],
-				arr[aux[i].sec], arr[aux[j].first],
-				arr[aux[j].sec]);
-			return;
-		}
-		else if (aux[i].sum + aux[j].sum < X)
-			i++;
-		else
-			j--;
-	}
-}
-
-
-int main()
-{
-	int arr[] = { 10, 20, 30, 40, 1, 2 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int X = 91;
-	
-	
-	findFourElements(arr, n, X);
-	return 0;
+int main(){
+    char* s = (char *)malloc(10240 * sizeof(char));
+    scanf("%s",s);
+    int coun=0,i;
+    for(i=0;i<strlen(s);i++)
+    {
+        if(s[i]>=65 && s[i]<=90){coun++;}
+    }
+    printf("%d\n",coun+1);
+    return 0;
 }
